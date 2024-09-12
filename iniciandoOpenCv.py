d1bd22a640fa0d6aef95126df2fc373ad20febac
Cleber-0101 <cleberbatista99@hotmail.com>
2024-09-12 00:52:56 -0300

    feat: projeto Abrindo imagem com openCv

---
+++ iniciandoOpenCv.py
@@ -0,0 +1,9 @@
+import cv2
+
+img =  cv2.imread('farol.jpg')
+imgCinza = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+#print(img.shape)
+cv2.imshow('imagem', img)
+cv2.imshow('imagem cinza', imgCinza)
+cv2.waitKey(0)
